e609742790fdcd62b9eeb22e25fe55048aaac82b
Jon Canning <jon.canning@gmail.com>
2018-12-27 17:18:51 +0000

    use giraffe services

--- demo/README.md
+++
@@ -1,8 +0,0 @@
-## Demo
-
-- Create a new function app
-- Open `deploy.sh`
-- Replace `{APP}` with your app name
-- Replace `{USER}` with your deployment user name
-- Replace `{PASSWORD}` with your deployment password
-- Run `./deploy.sh`
--- demo/Run.fs
+++ demo/Run.fs
@@ -3,6 +3,18 @@ module Run
 open Giraffe
 open Microsoft.AspNetCore.Http
 open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Extensions.Http
+open System.Threading.Tasks
+open FSharp.Control.Tasks.V2
+open Giraffe.DotLiquid
+open Microsoft.Extensions.Logging
+
+[<CLIMutable>] //For XML serialization
+type Person =
+  { FirstName : string
+    LastName  : string }
+
+let fooBar = { FirstName = "Foo"; LastName = "Bar" }
 
 let app : HttpHandler =
 
@@ -10,6 +22,18 @@ let app : HttpHandler =
 
     GET >=> route "/api/demo" >=> htmlFile "index.htm"
 
+    GET >=> route "/api/demo/foo" >=> negotiate fooBar
+
+    GET >=> route "/api/demo/person" >=> dotLiquidTemplate "text/html" "Templates/Person.liquid" fooBar
+
   ]
 
-let run (req : HttpRequest, context : ExecutionContext) = AzureFunctions.run req context app
+[<FunctionName "Giraffe">]
+let run ([<HttpTrigger (AuthorizationLevel.Anonymous, Route = "{*any}")>] req : HttpRequest, context : ExecutionContext, log : ILogger) =
+  let hostingEnvironment = req.HttpContext.GetHostingEnvironment()
+  hostingEnvironment.ContentRootPath <- context.FunctionAppDirectory
+  let func = Some >> Task.FromResult
+  task {
+    let! _ = app func req.HttpContext
+    return ()
+  } :> Task
