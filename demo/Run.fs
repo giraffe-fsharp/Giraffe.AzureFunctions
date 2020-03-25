7cc846a404b371bfda1658b7dfbe2faf81d5b344
Tobias Burger <tobias_burger@hotmail.com>
2020-03-25 16:30:24 +0100

    Add sample errorHandler

--- demo/Run.fs
+++ demo/Run.fs
@@ -26,7 +26,14 @@ let app : HttpHandler =
 
     GET >=> route "/api/demo/person" >=> dotLiquidTemplate "text/html" "Templates/Person.liquid" fooBar
 
+    GET >=> route "/api/demo/failing" >=> warbler (fun _ -> failwith "FAILURE")
+
   ]
+
+let errorHandler (ex : exn) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse
+    >=> ServerErrors.INTERNAL_ERROR ex.Message
 
 [<FunctionName "Giraffe">]
 let run ([<HttpTrigger (AuthorizationLevel.Anonymous, Route = "{*any}")>] req : HttpRequest, context : ExecutionContext, log : ILogger) =
@@ -35,4 +42,10 @@ let run ([<HttpTrigger (AuthorizationLevel.Anonymous, Route = "{*any}")>] req : 
   let func = Some >> Task.FromResult
   { new Microsoft.AspNetCore.Mvc.IActionResult with
       member _.ExecuteResultAsync(ctx) = 
-        app func ctx.HttpContext :> Task }
+        task {
+          try
+            return! app func ctx.HttpContext :> Task
+          with exn ->
+            return! errorHandler exn log func ctx.HttpContext :> Task
+        }
+        :> Task }
