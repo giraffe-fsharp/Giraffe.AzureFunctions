6d0fc3d2071c542f7a2c7cd1d0394f7fef07ad20
Tobias Burger <tobias_burger@hotmail.com>
2020-03-25 16:29:59 +0100

    Code formatting

--- demo/Startup.fs
+++ demo/Startup.fs
@@ -4,9 +4,9 @@ open Microsoft.Azure.Functions.Extensions.DependencyInjection
 open Giraffe
 
 type WebJobsExtensionStartup () =
-   inherit FunctionsStartup ()
-   override __.Configure(builder: IFunctionsHostBuilder) =
-       builder.Services.AddGiraffe() |> ignore
+  inherit FunctionsStartup ()
+  override __.Configure(builder: IFunctionsHostBuilder) =
+    builder.Services.AddGiraffe() |> ignore
 
 [<assembly: FunctionsStartup(typeof<WebJobsExtensionStartup>)>]
 do ()
